005316990d6e1124a720cad46a1ab00f0fe787c7
Muhammad Bilal <billu97.memon@gmail.com>
2020-07-20 00:39:23 +0500

    Completed this project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-import React from 'react';
-// import useWebAnimations from '@wellyshen/use-web-animations'
+import React, { useEffect } from 'react';
+import useWebAnimations from '@wellyshen/use-web-animations'
 import './App.css';
 
 function App() {
+  const birds = useWebAnimations({
+    keyframes: [
+      { transform: "translateX(-1000px)" },
+    ],
+    timing: {
+      duration: 10000,
+      iterations: Infinity,
+      easing: "linear",
+    }
+  });
+  const dog = useWebAnimations({
+    keyframes: [
+      { transform: "translateX(1100px)" },
+    ],
+    timing: {
+      duration: 10000,
+      iterations: Infinity,
+      easing: "linear",
+    }
+  });
+
+
+  const pauseAnimation = () => {
+    birds.getAnimation().pause();
+    dog.getAnimation().pause();
+  }
+  const startAnimation = () => {
+    birds.getAnimation().play();
+    dog.getAnimation().play();
+  }
+  const reverseAnimation = () => {
+    birds.getAnimation().reverse();
+    dog.getAnimation().reverse();
+  }
+  useEffect(() => {
+
+  }, [birds, dog])
+
   return (
     <>
       <div className="main-container">
-        <div className="flyingBirds">
-        </div>
-        <div className="runningDog"></div>
+        <div className="flyingBirds" ref={birds.ref}></div>
+        <div className="runningDog" ref={dog.ref}></div>
       </div >
-      <button>Check This</button>
+      <button onClick={pauseAnimation}>Pause</button>
+      <button onClick={startAnimation}>Start</button>
+      <button onClick={reverseAnimation}>Reverse</button>
     </>
   );
 };
