260ad742831da7933d8ae06637b85e499ffba92f
Muhammad Bilal <billu97.memon@gmail.com>
2020-07-19 21:57:25 +0500

    Initial State Has started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import useWebAnimations from '@wellyshen/use-web-animations'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="runningDog"></div>
+      <div className="runningMan"></div>
     </div>
   );
-}
-
+};
 export default App;
